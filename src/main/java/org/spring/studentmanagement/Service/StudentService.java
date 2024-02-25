892a70bb18a0278e2f1a523288d56286b634123c
Gyan <gyanbikashmili@gmail.com>
2024-02-25 19:36:48 +0530

    Files added

---
+++ src/main/java/org/spring/studentmanagement/Service/StudentService.java
@@ -0,0 +1,17 @@
+package org.spring.studentmanagement.Service;
+
+import org.spring.studentmanagement.Entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
+}
